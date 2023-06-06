c9a8acad56f1f1adc8debcfb7bbfc053dfced72f
Lavender <lavenderanyango9@gmail.com>
2023-06-06 12:12:10 -0700

    task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -3,4 +3,4 @@
 # Loop through numbers from 0 to 98
 for i in range(99):
     # Print the decimal number and its hexadecimal using string formatting
-    print(f"{i} = {hex(i)}")
+    print("{} = 0x{:0x}".format(i, i))
