334f4720c1238e1e2098e408b8b6f84892bf601e
Lavender <lavenderanyango9@gmail.com>
2023-06-06 12:31:21 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 # Loop through numbers from 0 to 99
-for i in range(1, 100):
+for i in range(100):
     # print the number with two digits, followed by a comma and space
     print("{:02d}, ".format(i) if i < 99 else "{:02d}\n".format(i), end="")
