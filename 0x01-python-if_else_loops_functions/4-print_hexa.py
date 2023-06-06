8f2c19c2b5d3a0b4b8e5eacda5f4a79dc6200c4e
Lavender <lavenderanyango9@gmail.com>
2023-06-06 11:55:54 -0700

    task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -3,4 +3,4 @@
 # Loop through numbers from 0 to 98
 for i in range(99):
     # Print the decimal number and its hexadecimal using string formatting
-    print("Decimal: {:2d} | Hexadecimal: {:2x}".format(i, i))
+    print("{:2d} = {:2x}".format(i, i))
