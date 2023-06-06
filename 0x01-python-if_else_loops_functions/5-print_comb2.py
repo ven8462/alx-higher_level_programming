d42deb6c7446775256b44b1045ba570d5f329611
Lavender <lavenderanyango9@gmail.com>
2023-06-06 12:29:33 -0700

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 # Loop through numbers from 0 to 99
-for i in range(100):
+for i in range(1, 100):
     # print the number with two digits, followed by a comma and space
-    if i < 99:
-        print("{:02d}, ".format(i), end="")
-    # If it's the last number, print the number with two digits and a newline
-    else:
-        print("{:02d}".format(i))
+    print("{:02d}, ".format(i) if i < 99 else "{:02d}\n".format(i), end="")
