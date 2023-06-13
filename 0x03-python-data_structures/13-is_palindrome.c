dd72629ad5120b61bc0eff117b94565d2c1eb6d3
Lavender <lavenderanyango9@gmail.com>
2023-06-12 19:31:13 -0700

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,62 +1,43 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
-
-/**
- * reverse_list - reverses a singly linked list
- * @head: pointer to the head of the list to be reversed
- * Return: pointer to the head of the reversed list
- */
-listint_t *reverse_list(listint_t *head)
-{
-	listint_t *prev = NULL, *next = NULL;
-
-	while (head != NULL)
-	{
-		next = head->next;
-		head->next = prev;
-		prev = head;
-		head = next;
-	}
-
-	return (prev);
-}
+#include "lists.h"
 
 /**
  * is_palindrome - checks if a singly linked list is a palindrome
- * @head: pointer to the head of the list to be checked
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
+ * @head: double pointer to the head of the linked-list
+ * Return: 1 if a palindrome. 0 otherwise
+*/
+
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-	{
+	if (!head || !*head)
 		return (1);
-	}
 
-	listint_t *slow = *head, *fast = *head, *prev_slow = NULL;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *reversed_half = NULL;
+	listint_t *temp;
 
-	while (fast != NULL && fast->next != NULL)
+	while (fast && fast->next)
 	{
 		fast = fast->next->next;
-		prev_slow = slow;
+		temp = slow;
+		slow = slow->next;
+		temp->next = reversed_half;
+		reversed_half = temp;
+	}
+
+	if (fast)
+		slow = slow->next;
+
+	while (reversed_half && slow)
+	{
+		if (reversed_half->n != slow->n)
+			return (0);
+
+		reversed_half = reversed_half->next;
 		slow = slow->next;
 	}
 
-	prev_slow->next = NULL;
-	listint_t *second_half = reverse_list(slow);
-	listint_t *second_half_copy = second_half;
-
-	while (*head != NULL && second_half != NULL)
-	{
-		if ((*head)->n != second_half->n)
-		{
-			reverse_list(second_half_copy);
-			return (0);
-		}
-		*head = (*head)->next;
-		second_half = second_half->next;
-	}
-
-	reverse_list(second_half_copy);
 	return (1);
 }
