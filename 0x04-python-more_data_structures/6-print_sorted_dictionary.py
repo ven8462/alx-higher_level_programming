b0ab729998b2ffd793f75517549e7861f625bfc6
Lavender <lavenderanyango9@gmail.com>
2023-06-14 11:22:58 -0700

    adding a condition

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    for key, value in sorted(a_dictionary.items()):
-        print("{}: {}".format(key, value))
+    if a_dictionary is not None:
+        for key, value in sorted(a_dictionary.items()):
+            print("{}: {}".format(key, value))
