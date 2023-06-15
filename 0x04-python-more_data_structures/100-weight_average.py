70a3d581643f45b7dbae1b215cc1a966872b32bc
Lavender <lavenderanyango9@gmail.com>
2023-06-15 12:55:47 -0700

    getting average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if not my_list or not isinstance(my_list, list):
         return 0
 
-    weight_sum = 0
+    weighted_sum = 0
     total_weight = 0
 
-    for score, weight in my_list:
+    for item in my_list:
+        if not isinstance(item, tuple) or len(item) != 2 or
+        not (isinstance(item[0], int) and isinstance(item[1], int)):
+            return 0
+        score, weight = item
         weighted_sum += score * weight
         total_weight += weight
 
-        return (weighted_sum / total_weight)
+    if total_weight == 0:
+        return 0
+
+    return (weighted_sum / total_weight)
