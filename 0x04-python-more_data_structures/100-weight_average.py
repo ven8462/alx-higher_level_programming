38c5ce956e9b3a0e42a22f0031657765b7a2e7b0
Lavender <lavenderanyango9@gmail.com>
2023-06-15 13:03:39 -0700

    correcting weight_average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if not my_list or not isinstance(my_list, list):
+    if not my_list:
         return 0
 
-    weighted_sum = 0
-    total_weight = 0
+    total_scores = sum(weight * score for score, weight in my_list)
+    total_weight = sum(weight for _, weight in my_list)
 
-    for item in my_list:
-        if not isinstance(item, tuple) or len(item) != 2 or
-        not (isinstance(item[0], int) and isinstance(item[1], int)):
-            return 0
-        score, weight = item
-        weighted_sum += score * weight
-        total_weight += weight
-
-    if total_weight == 0:
-        return 0
-
-    return (weighted_sum / total_weight)
+    return (total_scores / total_weight)
