528b5566a6f201cb0e12fb7d952dd0339ce6607e
lavender <lavenderanyango9@gmail.com>
2023-06-26 16:49:09 +0300

    prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        elements_printed = 0
+
+        for i in range(x):
+            print("{}".format(my_list[i]), end='')
+            elements_printed += 1
+
+    except (IndexError):
+        pass
+    print()
+    return elements_printed
