01ea0cdd75839971c3a4be0fe68787638d3e9e3a
lavender <lavenderanyango9@gmail.com>
2023-06-28 00:01:50 +0300

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,10 +4,10 @@
 
 
 class Square:
-    """Defines a square.
-    """
+    """Defines a square."""
+
     def __init__(self, size=0, position=(0, 0)):
-        """Initializes the square with an optional size and position."""
+        """Initializes the square."""
         self.size = size
         self.position = position
 
@@ -18,8 +18,12 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """Sets the size ensuring it is a non-negative integer."""
-        # Size setter code remains the same
+        """Sets the size ensuring it's a non-negative integer."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     @property
     def position(self):
@@ -28,13 +32,22 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """Sets the position ensuring it is   2 non-negative integers."""
-        # if it is write code raise an exception
+        """Sets the position ensuring it's a tuple of 2 non-ve integers."""
+        if not isinstance(value, tuple) or len(value) != 2 or\
+           not all(isinstance(i, int) and i >= 0 for i in value):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """Calculates and returns the current square's area."""
-        # Area code remains the same
+        return self.__size * self.__size
 
     def my_print(self):
-        """Prints the square considering its position."""
-        # Update print method to consider position
+        """Prints the square."""
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            print("\n" * self.__position[1], end="")
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
