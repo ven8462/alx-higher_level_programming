499f3a5d54cb866d08a59fc77598d7c82f8111cc
lavender <lavenderanyango9@gmail.com>
2023-06-27 21:30:10 +0300

    exceptions in classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
 
-"""Size Validation"""
+"""defines a class square with attributes"""
 
 
 class Square:
     """Defines a class Square"""
     def __init__(self, size=0):
         """Initializes the square with size.
-
         Args:
         size(int, optional): Size of the square is 0.
-
         Raises:
             TypeError: If size is not int.
             ValueError: If size is less than 0.
         """
 
-        if not (isinstance(size, int)):
+        if (not isinstance(size, int)):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size most be >= 0")
