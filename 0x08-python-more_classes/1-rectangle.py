27e70e8d93926d354c2e7788ac309861bd7d1914
lavender <lavenderanyango9@gmail.com>
2023-07-03 17:45:07 +0300

    correcting indetation error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,33 +14,32 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            """gets the width"""
-            return self.__width
+    @property
+    def width(self):
+        """gets the width"""
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """sets width"""
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            elif value < 0:
-                raise ValueError("width must be >= 0")
-            else:
-                self.__width = value
+    @width.setter
+    def width(self, value):
+        """sets width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
 
-            @property
-            def height(self):
-                """gets the height"""
-                return self.__height
+    @property
+    def height(self):
+        """gets the height"""
+        return self.__height
 
-            @height.setter
-            def height(self, value):
-                """sets height"""
-                if not isinstance(value, int):
-                    raise TypeError("height must be an integer")
-                elif value < 0:
-                    raise ValueError("height must be >= 0")
-                else:
-                    self.__height = value
-                    raisa
+    @height.setter
+    def height(self, value):
+        """sets height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
