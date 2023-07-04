eda8a2035e8d5309056e46c032fdb554a24a2c90
lavender <lavenderanyango9@gmail.com>
2023-07-04 18:27:21 +0300

    correction

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""locked class with no class or attribute"""
+
+
 class LockedClass:
+    """locked class"""
     def __setattr__(self, name, value):
         if name != "first_name":
             msg = "'LockedClass' object has no attribute 'last_name'"
