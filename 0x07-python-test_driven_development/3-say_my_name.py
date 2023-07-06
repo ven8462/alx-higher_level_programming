44376bf2fc0d446aaa2f28e60d1a5ef97d9e4b7f
lavender <lavenderanyango9@gmail.com>
2023-07-06 12:15:21 +0300

    tests for task3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """
+    This function receives a first name and last name and print them
+    """
+    # These first two conditionals check if 'first_name' and 'last_name'
+    # are strings. If they're not, the function raises a TypeError.
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    # This is where the function prints out the statement.
+    print("My name is {:s} {:s}".format(first_name, last_name))
