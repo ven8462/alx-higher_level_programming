7cfd3c8cad736ebaaefd8134201bec6dba98903f
lavender <lavenderanyango9@gmail.com>
2023-07-10 16:26:53 +0300

    lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a function taht takes an obj as an argument
+and returns a list of it's available attributes
+"""
+
+
+def lookup(obj):
+    """ it works the built_in function dir()"""
+    return dir(obj)
