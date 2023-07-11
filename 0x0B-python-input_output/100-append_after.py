b4e22b2e5f808966f206d77332c3c6da7a3b6b28
lavender <lavenderanyango9@gmail.com>
2023-07-11 23:15:30 +0300

    making corrections

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,13 +5,13 @@ after each line containing a specific string"""
 
 def append_after(filename, search_string, new_string):
     """inserts a line of text to a file"""
-    with open(filename, 'r') as file:
+    text = []
+    with open(filename, 'r+', encoding='utf-8') as fw:
         text = file.readlines()
 
-    with open(filename, 'w') as fw:
-        string = ""
-        for line in text:
-            string += line
-            if search_string in line:
-                string += new_string
-                fw.write(string)
+    with open(filename, 'w', encoding='utf-8') as fw:
+        for lines in text:
+            fw.write(lines)
+
+            if search_string in lines:
+                fw.write(new_string)
