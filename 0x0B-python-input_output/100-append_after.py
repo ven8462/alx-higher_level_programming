697a0e9039458cbfe37b12cf5b7b4def45273e42
lavender <lavenderanyango9@gmail.com>
2023-07-11 23:08:05 +0300

    correct task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,15 +3,15 @@
 after each line containing a specific string"""
 
 
-def insert_line_after_string(filename, search_string, new_line):
+def append_after(filename, search_string, new_string):
     """inserts a line of text to a file"""
     with open(filename, 'r') as file:
-        lines = file.readlines()
+        text = file.readlines()
 
-    for i, line in enumerate(lines):
-        if search_string in line:
-            lines.insert(i + 1, new_line + '\n')
-            break
-
-    with open(filename, 'w') as file:
-        file.writelines(lines)
+    with open(filename, 'w') as fw:
+        string = ""
+        for line in text:
+            string += line
+            if search_string in line:
+                string += new_string
+                fw.write(string)
