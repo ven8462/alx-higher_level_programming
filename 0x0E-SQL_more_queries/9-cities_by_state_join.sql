9be1bd73e81e4c0c252e2ced0d895613e0b34272
lavender <lavenderanyango9@gmail.com>
2023-08-16 16:28:39 +0300

    task9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,2 @@
 -- lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.state_id = state.id
-ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id;
