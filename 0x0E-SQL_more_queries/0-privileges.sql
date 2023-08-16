35fac60d5e83b92c54f9a2fdd99fc962ed35bcda
lavender <lavenderanyango9@gmail.com>
2023-08-16 12:29:25 +0300

    creating second user

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- you need to create the users first
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
