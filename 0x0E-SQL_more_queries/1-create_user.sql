6904423d630dcca8519c9f8acccb223082a44e43
lavender <lavenderanyango9@gmail.com>
2023-08-16 13:36:27 +0300

    root user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- creates the MySQL server user and grants permissions
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
