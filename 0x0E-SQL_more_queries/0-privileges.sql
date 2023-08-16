a8ce45053c7afcf2d94f0e5cb7586c2479c8f4f8
lavender <lavenderanyango9@gmail.com>
2023-08-16 12:50:07 +0300

    deleted the users

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
--- you need to create the users first
+-- List all privileges of MySQL users
+-- created the users first
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
