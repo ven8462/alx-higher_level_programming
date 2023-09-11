7a6b60e3e811e9acb6576561a5ce4b8222360876
lavender <lavenderanyango9@gmail.com>
2023-09-11 17:18:28 +0300

    correcting num5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,8 +3,8 @@
 const firstArgv = process.argv[2];
 const num = parseInt(firstArgv);
 
-if (isNaN(num)) {
-  console.log('Not a number');
+if (!isNaN(num)) {
+  console.log('My number:', num);
 } else {
-  console.log('My number: ', num);
+  console.log('Not a number');
 }
