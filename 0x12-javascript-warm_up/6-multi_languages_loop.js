604d5e05ca449c875ae434ba74676c65cf3f4d69
lavender <lavenderanyango9@gmail.com>
2023-09-11 21:41:36 +0300

    task6 corrections

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i <= myArray.length; i++) {
+for (let i = 0; i < myArray.length; i++) {
   console.log(myArray[i]);
 }
