4c82831231fcf867927f42e5d7d56c781bef49f2
lavender <lavenderanyango9@gmail.com>
2023-09-16 11:51:23 +0300

    task0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) != 4:
+        print("usage: {} username password database_name".format(args[0]))
+    exit(1)
+
+    username = args[1]
+    password = args[2]
+    data = args[3]
+
+    db = MySQLdb.connect(host='localhost',
+                         user=username,
+                         passwd=password,
+                         db=data, port=3306)
+
+    cur = db.cursor()
+    num_rows = cur.execute('SELECT * FROM states ORDER BY states.id;')
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
