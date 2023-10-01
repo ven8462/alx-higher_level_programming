bbf34d1a87cb7ff1fe3ac8ae4a37279870a4afc5
lavender <lavenderanyango9@gmail.com>
2023-10-01 15:35:28 +0300

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches
+https://alx-intranet.hbtn.io/status"""
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        bytes_content = response.read()
+        content = bytes_content.decode('utf-8')
+        string = 'Body response:\n\t- type: {}\n\t- content: {}\n\t- \
+utf8 content: {}'.format(type(bytes_content), bytes_content, content)
+        print(string)
