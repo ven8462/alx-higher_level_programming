fa1e9e308feb3dad5237bf69c2a52ed3122c5b9d
lavender <lavenderanyango9@gmail.com>
2023-10-01 16:33:41 +0300

    task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Python script that takes in a URL and an email,
+sends a POST request to the passed URL
+"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = argv[1]
+    email = {'email': argv[2]}
+    email = urlencode(email)
+    email = email.encode('ascii')
+    request = Request(url, email)
+    with urlopen(request) as response:
+        string = response.read().decode('utf-8')
+        print(string)
