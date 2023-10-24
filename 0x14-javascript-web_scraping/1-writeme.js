234d4fb8c51bbf152ddc348b67abcf346b2cb908
lavender <lavenderanyango9@gmail.com>
2023-10-24 12:40:50 +0300

    fix writeme

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -9,12 +9,8 @@ const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
 
 // Write the string to the file
-fs.writeFile(filePath, stringToWrite, 'utf8', (err) => {
-  if (err) {
-    // If an error occurred, print the error object
-    console.error(err);
-  } else {
-    // If no error occurred, print a success message
-    console.log(stringToWrite);
+fs.writeFile(filePath, stringToWrite, 'utf8', function (error, data) {
+  if (error) {
+    console.log(error);
   }
 });
